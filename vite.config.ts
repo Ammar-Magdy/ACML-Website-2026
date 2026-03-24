13d19324db3cfdc46c8d7dc304833c97b8187aaf
Ammar-Magdy <A.M.FATHALLA@student.aast.edu>
2026-03-24 23:31:56 +0200

    bb

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ACML/",
+  base: "/ACML-Website-2026/",
   assetsInclude: ["**/*.docx", "**/*.pptx"],
   optimizeDeps: {
     exclude: ["lucide-react"],
